492bd42ca97487516e6a6bf99f024fa0da739c14
alifanuraniputri <alifanuraniputri@gmail.com>
2015-12-04 22:59:27 +0700

    first init, base REST API with Grails

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,25 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+	
+	'app-js' {
+		dependsOn 'angular'  // base angular modules
+        resource url:'js/app.js'
+        resource url:'js/services.js'
+        resource url:'js/controllers.js'
+        resource url:'js/filters.js'
+        resource url:'js/directives.js'
+    }
+
+    'app' {
+		// dependsOn 'angular'  // base angular modules
+		// dependsOn 'angular-top'  // most common angular modules
+		// dependsOn 'angular-all'  // all angular modules (without test scripts)
+		dependsOn 'angular-all'  // enable this, as a sample ...
+		dependsOn 'app-js'  // application-specific angular-related scripts ...
+    }
+
+    'index' {
+        // resource url:'js/app.js'
+    }
 }
---
+++ test/unit/grails/angularjs/resources/test/AngularSampleGrailsControllerSpec.groovy
@@ -0,0 +1,20 @@
+package grails.angularjs.resources.test
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(AngularSampleGrailsController)
+class AngularSampleGrailsControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
